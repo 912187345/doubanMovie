8aafc1f4bbe69d34455fee9981f295c5eda4122f
912187345 <912187345@qq.com>
2017-10-08 23:05:44 +0800

    增加图书板块路由

--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,9 @@ import Router from 'vue-router'
 
 Vue.use(Router)
 
+// 首页
+import index from '../movie/choseModul.vue'
+
 // movie
 import mIndex from '../movie/pages/index'
 import celebrity from '../movie/pages/celebrityDetail'
@@ -12,52 +15,76 @@ import list1 from '../movie/components/list1'
 import list2 from '../movie/components/list2'
 import list3 from '../movie/components/list3'
 
+// book
+import bIndex from '../movie/bookPage/index'
+import bDetail from '../movie/bookPage/bookDetail'
+
 export default new Router({
   mode: 'history',
   routes: [
+    // 首页
     {
-      path: '/',
+      path:'/',
+      name:'index',
+      component:index
+    },
+    // 电影 S
+    {
+      path: '/movie',
       name: 'mIndex',
       component: mIndex,
       meta: { keepAlive: true },
-      redirect:'/piaofang',
       children:[{
-        path:'/piaofang',
-        name:list1,
+        path:'piaofang',
+        name:'list1',
         component:list1,
         meta: { keepAlive: true }
       },
       {
-        path:'/shangying',
-        name:list2,
+        path:'shangying',
+        name:'list2',
         component:list2,
         meta: { keepAlive: true }
       },{
-        path:'/TOP250',
-        name:list3,
+        path:'TOP250',
+        name:'list3',
         component:list3,
         meta: { keepAlive: true }
       }]
     },
     {
-      path: '/search',
+      path: '/search/:type',
       name: 'search',
       component: mSearch
     },
+    // {
+    //   path: '/movie/detail',
+    //   name: 'mDetail',
+    //   component: mDetail
+    // },
     {
-      path: '/detail',
-      name: 'mDetail',
-      component: mDetail
-    },
-    {
-      path: '/movieDetail',
+      path: '/movie/movieDetail',
       name: 'movieDetail',
       component: mDetail
     },
     { //影人详情
-      path: '/celebrity',
+      path: '/movie/celebrity',
       name: 'celebrity',
       component: celebrity
+    },
+    // 电影 E
+
+    // 图书 S
+    {
+      path:'/book',
+      name:'bIndex',
+      component: bIndex
+    },
+    {
+      path:'/book/:bookId',
+      name:'bDetail',
+      component: bDetail
     }
+    // 图书 E
   ]
 })
