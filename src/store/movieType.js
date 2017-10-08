f97d108ffb448e6617b0626e9bb8564b8e5f0e5d
912187345 <912187345@qq.com>
2017-10-08 23:02:13 +0800

    store增加书本字段

--- src/store/movieType.js
+++ src/store/movieType.js
@@ -1,10 +1,16 @@
 
 // mutstions types
+// 电影
 export const TOGGLE_MOVIE_DETAIL = 'TOGGLE_MOVIE_DETAIL' // 电影详情
 export const TOGGLE_MOVIE_CELEBRITY = 'TOGGLE_MOVIE_CELEBRITY' // 影人详情
+// 书本
+export const TOGGLE_BOOK_DETAIL = 'TOGGLE_BOOK_DETAIL' // 书本详情
 
 // actions types
+// 电影
 export const FETCH_MOVIE_DETAIL = 'FETCH_MOVIE_DETAIL' //电影详情
 export const FETCH_MOVIE_CELEBRITY = 'FETCH_MOVIE_CELEBRITY' // 影人详情
+// 书本
+export const FETCH_BOOK_CELEBRITY = 'FETCH_BOOK_CELEBRITY' // 影人详情
 
 // getters types
